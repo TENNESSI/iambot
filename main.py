e58379d5770874439dfaba06836b061a87435182
TENNESSI <bratuhin-kostya99@mail.ru>
2025-05-22 12:40:38 +0400

    Закомментиовал код, добавил статус тайпинга, ожидание имитация чтения сообщения
    Signed-off-by: TENNESSI <bratuhin-kostya99@mail.ru>

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from telethon import TelegramClient, events
-from config import api_id, api_hash
+from config import api_id, api_hash, api_key
 import asyncio
 
 client = TelegramClient('test_session', api_id, api_hash)
@@ -7,10 +7,28 @@ client = TelegramClient('test_session', api_id, api_hash)
 @client.on(events.NewMessage(incoming=True))
 async def message_handler(event):
 	sender = await event.get_sender()
-	print(f'[Сообщение] от {sender.first_name}: {event.text}')
+	
+	#Проверка является ли сообщение личным сообщением от пользователя
+	# if event.is_private and sender.id == 1020139714:
+	if event.is_private:
+		#присвоение переменным айди, имя, текст сообщения их значений
+		user_id = sender.id
+		name = sender.first_name
+		message_text = event.text
 
-	if event.is_private and sender.id == 1020139714:
-		await asyncio.sleep(3)
+		#Вывод в консоль всех личных сообщений
+		print(f'[Сообщение] от {name}: {message_text}')
+		
+		time_to_read = len(message_text) / 15 #Вычисление времени чтения сообщения
+		time_to_typing = len(answer_message) / 4 #Вычисление времени печатаня ответа
+		
+		await asyncio.sleep(time_to_read) #афк время, имитация чтения сообщения
+
+		#статус "печатает..."
+		async with client.action(user_id, 'typing'):
+			await asyncio.sleep(time_to_typing)
+
+		#отправка ответа
 		await event.reply(f'Бот:\n{event.text}\nна её трупе растёт сад')
 
 
